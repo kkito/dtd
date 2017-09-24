66792f22e224feacbfebc2a3cbc537ec505f9793
kk.shen <kk.shen@saybot.com>
2017-09-24 20:10:52 +0800

    add parser class

---
+++ __tests__/parser-spec.ts
@@ -0,0 +1,24 @@
+import {Parser} from '../src/parser';
+
+test('.matchComments' , () => {
+    const firstComment = '<!-- xxxsdfwef -->'
+    const secondComment = '<!-- swfewf -->'
+    const result = Parser.matchComments(`${firstComment} <!sswef > ${secondComment}`)
+    expect(result.length).toBe(2)
+    expect(result[1]).toBe(secondComment)
+    expect(result[0]).toBe(firstComment)
+})
+
+test('.matchDTDTags' , () => {
+    const firstTag = '<!ELEMENT smilCustomTest EMPTY>'
+    const secondTag = "<!ATTLIST smilCustomTest \
+    id    ID    #REQUIRED \
+    defaultState  (true|false)   'false' \
+    override  (visible|hidden) 'hidden' \
+    bookStruct  (PAGE_NUMBER|NOTE|NOTE_REFERENCE|ANNOTATION|LINE_NUMBER|OPTIONAL_SIDEBAR|OPTIONAL_PRODUCER_NOTE)  #IMPLIED \
+    >"
+    const tags = Parser.matchDTDTags(` place all customTest elements used in the SMIL files, for presentation to the user. ${firstTag} ${secondTag}`)
+    expect(tags.length).toBe(2)
+    expect(tags[0]).toBe(firstTag)
+    expect(tags[1]).toBe(secondTag)
+})
