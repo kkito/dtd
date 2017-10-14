3c7be350717dbbdf1c9a1c6fb3e68c50f57867bc
kkito <kkito@sohu.com>
2017-10-14 14:11:06 +0800

    element diff from elementContent and category

--- __tests__/tag/element-spec.ts
+++ __tests__/tag/element-spec.ts
@@ -3,6 +3,7 @@ import { Element } from '../../src/tag/element';
 const eleStr = '<!ELEMENT square EMPTY>';
 const eleStr2 = '<!ELEMENT testTagName (name,age,school)>';
 const ele = new Element(eleStr);
+const ele2 = new Element(eleStr2);
 
 test('initialize', () => {
   expect(ele).not.toBeNull();
@@ -10,6 +11,14 @@ test('initialize', () => {
 
 test('get tag name', () => {
   expect(ele.getName()).toBe('square');
-  const ele2 = new Element(eleStr2);
   expect(ele2.getName()).toBe('testTagName');
 });
+
+
+test('#belongsCategory , #hasElementContent' , () => {
+  expect(ele.belongsCategory()).toBeTruthy()
+  expect(ele.hasElementContent()).toBeFalsy()
+  expect(ele2.belongsCategory()).toBeFalsy()
+  expect(ele2.hasElementContent()).toBeTruthy()
+})
+
--- src/tag/element.ts
+++ src/tag/element.ts
@@ -1,15 +1,48 @@
 import { Tag } from './tag';
+import {ElementContent} from './element_content';
 /**
  * Element 
- * <!ELEMENT 元素名称 类别>
+ * <!ELEMENT element-name category>
  * or
- * <!ELEMENT 元素名称 (元素内容)>
+ * <!ELEMENT element-name (element-content)>
  */
 export class Element extends Tag {
+
+  public static readonly CATEGORY_EMPTY = 'EMPTY'
+  public static readonly CATEGORY_ANY = 'ANY'
+
+  private theElementContent:string;
+
+  public constructor(tag: string) {
+    super(tag);
+    let matches = this.elementValue().match(/\((.+)\)/);
+    if(matches){
+      this.theElementContent = matches[0]
+    }
+  }
+
   /**
    * the tag name always this first string of contents
    */
   public getName(): string {
     return this.contents[0];
   }
+
+  public belongsCategory():boolean {
+    return !this.theElementContent;
+  }
+
+  public hasElementContent():boolean {
+    return !this.belongsCategory();
+  }
+
+  public elementContents():ElementContent[] {
+    //TODO
+    return []
+  }
+
+  protected elementValue():string {
+    return this.contents[1]
+  }
+
 }
