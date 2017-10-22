fc5584ec7fa0a59982b1884d225eb236d3df1aff
kkito <kkito@sohu.com>
2017-10-22 09:10:00 +0800

    add some interface for doctype

---
+++ src/tag/doctype.ts
@@ -0,0 +1,30 @@
+export interface IDoctypeAttribute {
+    getName():string;
+}
+
+export interface IDoctypeAttributeValue{
+    getValue():string;
+}
+
+export interface IDoctype {
+    getName():string;
+    getAttributeName():string;
+    getAttributeType():IDoctypeAttribute;
+    getAttributeValue():string;
+}
+
+export class DocType {
+    public static attributeTypes():IDoctypeAttribute[] {
+        // TODO
+        return []
+    }
+
+    public static attributeValues():IDoctypeAttributeValue[] {
+        // TODO
+        return []
+    }
+
+    public getName():string {
+        return ""
+    }
+}
