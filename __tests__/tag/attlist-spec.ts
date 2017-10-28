b1858543130910d9f8ef949224aac966b156ecdc
kkito <kkito@sohu.com>
2017-10-29 07:57:19 +0800

    test of attlist fixed value

--- __tests__/tag/attlist-spec.ts
+++ __tests__/tag/attlist-spec.ts
@@ -1,7 +1,11 @@
-import { Attlist , AttributeType } from '../../src/tag/attlist';
+import { Attlist , AttributeType , AttributeValue} from '../../src/tag/attlist';
 
 const attlistStr1 = '<!ATTLIST payment type CDATA "check">';
-
+const attlistStr2 = '<!ATTLIST square width CDATA "0">'
+const attlistStr3 = '<!ATTLIST person number CDATA #REQUIRED>'
+const attlistStr4 = '<!ATTLIST contact fax CDATA #IMPLIED>'
+const attlistStrFixed = '<!ATTLIST sender company CDATA #FIXED "Microsoft">'
+const attlistStr6 = '<!ATTLIST payment type (check|cash) "cash">'
 
 test('initialize' , () => {
     const attlist = new Attlist(attlistStr1);
@@ -10,5 +14,12 @@ test('initialize' , () => {
     expect(attlist.getAttributeType()).toBe(AttributeType.CDATA)
     // TODO remove ""
     // expect(attlist.getAttributeValue()).toBe('check')
+})
+
+test('fixed data' , () => {
+    const  attlist = new Attlist(attlistStrFixed);
+    expect(attlist.getAttibuteName()).toBe('company')
+    expect(attlist.getAttributeType()).toBe(AttributeType.CDATA)
+    expect(attlist.getAttributeValue()).toBe(AttributeValue.FIXED)
 })
 
