652d5d9c34171cb65179ac589090219b49aa1b41
kkito <kkito@sohu.com>
2017-10-29 08:13:19 +0800

    add stirng utils

---
+++ __tests__/utils/string-util-spec.ts
@@ -0,0 +1,18 @@
+import { StringUtil } from '../../src/utils/string-util';
+
+test('.betweenQuotation' , () => {
+    let result = StringUtil.betweenQuotation('"name"')
+    expect(result).toBe('name')
+
+    result = StringUtil.betweenQuotation('""')
+    expect(result).toBe('')
+
+    result = StringUtil.betweenQuotation('ddd')
+    expect(result).toBe('')
+
+    result = StringUtil.betweenQuotation('\'"')
+    expect(result).toBe('')
+
+    result = StringUtil.betweenQuotation('"test"ddd"')
+    expect(result).toBe('test\"ddd')
+})
---
+++ src/utils/string-util.ts
@@ -0,0 +1,10 @@
+export class StringUtil {
+    public static betweenQuotation(origin: string): string {
+        const result = origin.match(/\"(.*)\"/)
+        if (!result) {
+            return ''
+        } else {
+            return result[1];
+        }
+    }
+}
